8b00b7547a55d16015d2b2922bb40e671a8ac1a0
Tim Brust <tim.brust@nordakademie.de>
2016-10-28 17:02:48 +0200

    feat: add missing relationships, add answer REST service, refactor: ids, services

--- src/main/java/de/nordakademie/iaa_multiple_choice/domain/Answer.java
+++ src/main/java/de/nordakademie/iaa_multiple_choice/domain/Answer.java
@@ -1,13 +1,11 @@
 package de.nordakademie.iaa_multiple_choice.domain;
 
 import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
-import org.hibernate.annotations.NaturalId;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,13 +16,12 @@ import lombok.Setter;
 public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "answer_id")
     private Long id;
 
     @Basic
-    private Boolean rightAnswer;
+    private boolean rightAnswer;
 
-    @ManyToOne(optional = false)
-    @NaturalId
-    private Question questionId;
-
+    @Basic
+    private String text;
 }
--- src/main/java/de/nordakademie/iaa_multiple_choice/service/ExamService.java
+++ src/main/java/de/nordakademie/iaa_multiple_choice/service/ExamService.java
@@ -8,29 +8,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import de.nordakademie.iaa_multiple_choice.domain.Exam;
 import de.nordakademie.iaa_multiple_choice.domain.ExamRepository;
-import de.nordakademie.iaa_multiple_choice.domain.Question;
-import de.nordakademie.iaa_multiple_choice.domain.QuestionRepository;
 
 @Service
 public class ExamService {
-
-    private final ExamRepository examRepository;
-    private final QuestionRepository questionRepository;
-
     @Autowired
-    public ExamService(final ExamRepository examRepository, final QuestionRepository questionRepository) {
-        this.examRepository = examRepository;
-        this.questionRepository = questionRepository;
-    }
+    private ExamRepository examRepository;
 
     @Transactional
     public void createExam(final Exam exam) {
         examRepository.createExam(exam);
     }
 
-    @Transactional
-    public List<Question> getQuestions(final Exam exam) {
-        return questionRepository.findQuestions(exam);
+    @Transactional(readOnly = true)
+    public Exam find(Long examId) {
+        return examRepository.find(examId);
     }
 
     @Transactional(readOnly = true)
