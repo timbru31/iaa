36f6b69bda170ff08013545c22c86d55f5f3fef9
Jens Gottwald <jens.gottwald@nordakademie.de>
2016-10-28 10:13:17 +0200

    JG: Verweise Answer -> Question -> Exam

--- src/main/java/de/nordakademie/iaa_multiple_choice/domain/Question.java
+++ src/main/java/de/nordakademie/iaa_multiple_choice/domain/Question.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.NaturalId;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,4 +29,7 @@ public class Question {
     @Basic
     private String text;
 
+    @ManyToOne(optional = false)
+    @NaturalId
+    private Exam exam;
 }
