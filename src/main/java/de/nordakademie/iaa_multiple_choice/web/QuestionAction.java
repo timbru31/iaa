156571d849bbf280eeb3ef8b638e21924d52f1c8
Tim Brust <tim.brust@nordakademie.de>
2016-10-30 19:14:59 +0100

    WIP: save answers when creating a question

--- src/main/java/de/nordakademie/iaa_multiple_choice/web/QuestionAction.java
+++ src/main/java/de/nordakademie/iaa_multiple_choice/web/QuestionAction.java
@@ -1,11 +1,15 @@
 package de.nordakademie.iaa_multiple_choice.web;
 
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import de.nordakademie.iaa_multiple_choice.domain.Answer;
 import de.nordakademie.iaa_multiple_choice.domain.Exam;
 import de.nordakademie.iaa_multiple_choice.domain.Question;
+import de.nordakademie.iaa_multiple_choice.service.AnswerService;
 import de.nordakademie.iaa_multiple_choice.service.ExamService;
 import de.nordakademie.iaa_multiple_choice.service.QuestionService;
 import de.nordakademie.iaa_multiple_choice.web.util.LecturerRequired;
@@ -21,6 +25,8 @@ public class QuestionAction extends ActionSupport {
     private QuestionService questionService;
     @Autowired
     private ExamService examService;
+    @Autowired
+    private AnswerService answerService;
     @Getter
     @Setter
     private Question question;
@@ -28,13 +34,31 @@ public class QuestionAction extends ActionSupport {
     @Setter
     private Long examId;
 
+    @Getter
+    @Setter
+    private String rawAnswerTextsSc[]; // SC || MC
+    // private String/boolean selected;
+    @Getter
+    @Setter
+    private Integer sc;
+    private Integer mc[];
+
+    private String rawFitbText; // der zweite weltrkrieg begann [lösugn]
+
     public String deleteQuestion() {
         questionService.deleteQuestion(question.getId());
         return SUCCESS;
     }
 
     public String saveQuestion() {
+        question.setAnswers(new HashSet<>());
         final Exam exam = examService.find(examId);
+        for (int i = 0; i < rawAnswerTextsSc.length; i++) {
+            final String rawAnswerText = rawAnswerTextsSc[i];
+            final Answer answer = new Answer(rawAnswerText, i == sc);
+            question.addAnswer(answer);
+            answerService.createAnswer(answer);
+        }
         questionService.createQuestion(question);
         exam.addQuestion(question);
         examService.updateExam(exam);
