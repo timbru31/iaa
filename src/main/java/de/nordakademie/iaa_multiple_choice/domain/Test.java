7b208e6c0aeec745566fd23cd82b49ae21228348
Tim Brust <tim.brust@nordakademie.de>
2016-11-03 03:04:42 +0100

    fix: remove test class

--- src/main/java/de/nordakademie/iaa_multiple_choice/domain/Test.java
+++
@@ -1,7 +0,0 @@
-package de.nordakademie.iaa_multiple_choice.domain;
-
-public class Test {
-    public static void main(final String[] args) {
-        System.out.println(CreditPointType.valueOf("HALF").getValue());
-    }
-}
